2c53ef705f35d44474b30552232469daaf8a95a0
Ishak <ishaakramadhan1305@gmail.com>
2020-05-29 21:14:36 +0800

    Hubungkan MainActivity dengan AddListActivity

--- app/src/main/java/id/ac/unhas/finaltodolist/MainActivity.kt
+++ app/src/main/java/id/ac/unhas/finaltodolist/MainActivity.kt
@@ -1,12 +1,27 @@
 package id.ac.unhas.finaltodolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var floatingActionButton: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        floatingActionButton = findViewById(R.id.fab)
+
+        floatingActionButton.setOnClickListener {
+            addList()
+        }
+    }
+
+    private fun addList() {
+        val addIntent = Intent(this, AddListActivity::class.java)
+        startActivity(addIntent)
     }
 }
