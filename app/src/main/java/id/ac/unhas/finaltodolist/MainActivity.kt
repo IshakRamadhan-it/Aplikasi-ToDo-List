5d7ed159c117d161697853ed627e954e06645ffe
Ishak <ishaakramadhan1305@gmail.com>
2020-05-29 21:24:12 +0800

    Membuat Fitur Alert Saat Menekan Salah Satu Tugas

--- app/src/main/java/id/ac/unhas/finaltodolist/MainActivity.kt
+++ app/src/main/java/id/ac/unhas/finaltodolist/MainActivity.kt
@@ -3,11 +3,19 @@ package id.ac.unhas.finaltodolist
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import id.ac.unhas.todolist.db.todolist.ToDoList
+import id.ac.unhas.todolist.ui.view_model.ToDoListViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var floatingActionButton: FloatingActionButton
+    private lateinit var toDoListViewModel: ToDoListViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,13 +23,40 @@ class MainActivity : AppCompatActivity() {
 
         floatingActionButton = findViewById(R.id.fab)
 
-        floatingActionButton.setOnClickListener {
+        listRV.layoutManager = LinearLayoutManager(this)
+        floatingActionButton.setOnClickListener{
             addList()
         }
     }
-
     private fun addList() {
         val addIntent = Intent(this, AddListActivity::class.java)
         startActivity(addIntent)
+    }
+    private fun showAlertMenu(toDoList: ToDoList){
+        val items = arrayOf("Info", "Ubah", "Hapus")
+
+        val builder = AlertDialog.Builder(this)
+        val alert = AlertDialog.Builder(this)
+        builder.setItems(items){ dialog, which ->
+            when(which){
+                0 -> {
+                }
+                1 -> {
+                }
+                2 -> {
+                    alert.setTitle("Hapus Tugas ?")
+                        .setMessage("Apakah anda yakin ?")
+                        .setPositiveButton("Iya"){dialog, _ ->
+                            toDoListViewModel.deleteList(toDoList)
+                            dialog.dismiss()
+                        }
+                        .setNegativeButton("Tidak"){dialog, _ ->
+                            dialog.dismiss()
+                        }
+                        .show()
+                }
+            }
+        }
+        builder.show()
     }
 }
